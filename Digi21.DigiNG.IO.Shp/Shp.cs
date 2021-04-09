8ede4db9ccce267a238a68983420bbf5ae381144
José Ángel <joseangelmt@outlook.com>
2021-04-09 07:16:25 +0200

    Refactorization

--- Digi21.DigiNG.IO.Shp/Shp.cs
+++ Digi21.DigiNG.IO.Shp/Shp.cs
@@ -13,40 +13,40 @@ namespace Digi21.DigiNG.IO.Shp
         public event ResultsAddedEventHandler ResultsAdded;
         public event TaskAddedEventHandler TaskAdded;
 
-        public Shp(string path) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab) { throw null; }
-        public Shp(string path, CodePage codePage) { throw null; }
-        public Shp(string path, DelegateGetWkt GetWkt) { throw null; }
-        public Shp(string path,  bool readOnly) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab, CodePage codePage) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab,  bool readOnly) { throw null; }
-        public Shp(string path, DelegateGetWkt GetWkt, CodePage codePage) { throw null; }
-        public Shp(string path, DelegateGetWkt GetWkt,  bool readOnly) { throw null; }
-        public Shp(string path,  bool readOnly, CodePage codePage) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt, CodePage codePage) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt,  bool readOnly) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab,  bool readOnly, CodePage codePage) { throw null; }
-        public Shp(string path, DelegateGetWkt GetWkt,  bool readOnly, CodePage codePage) { throw null; }
-        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt,  bool readOnly, CodePage codePage) { throw null; }
-        public ReadOnlyComplex Add(Complex complex) { throw null; }
-        public void Add(Entity entity) { throw null; }
-        public ReadOnlyLine Add(Line line) { throw null; }
-        public ReadOnlyPoint Add(Point point) { throw null; }
-        public ReadOnlyPolygon Add(Polygon polygon) { throw null; }
-        public ReadOnlyText Add(Text text) { throw null; }
-        public void Add(IEnumerable<Entity> entities) { throw null; }
-        public void Add(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes) { throw null; }
-        public void Delete(Entity entity) { throw null; }
-        public void Delete(IEnumerable<Entity> entities) { throw null; }
-        public void Dispose() { throw null; }
-        public IDictionary<string, IDictionary<string, object>> GetDatabaseAttributes(Entity entity) { throw null; }
-        public IEnumerator<Entity> GetEnumerator() { throw null; }
-        IEnumerator IEnumerable.GetEnumerator() { throw null; }
-        public string Wkt { get; }
-        public IDictionary<string, int> DatabaseTables { get; }
+        public Shp(string path) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab) => throw null;
+        public Shp(string path, CodePage codePage) => throw null;
+        public Shp(string path, DelegateGetWkt GetWkt) => throw null;
+        public Shp(string path,  bool readOnly) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, CodePage codePage) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab,  bool readOnly) => throw null;
+        public Shp(string path, DelegateGetWkt GetWkt, CodePage codePage) => throw null;
+        public Shp(string path, DelegateGetWkt GetWkt,  bool readOnly) => throw null;
+        public Shp(string path,  bool readOnly, CodePage codePage) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt, CodePage codePage) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt,  bool readOnly) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab,  bool readOnly, CodePage codePage) => throw null;
+        public Shp(string path, DelegateGetWkt GetWkt,  bool readOnly, CodePage codePage) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt,  bool readOnly, CodePage codePage) => throw null;
+        public ReadOnlyComplex Add(Complex complex) => throw null;
+        public void Add(Entity entity) => throw null;
+        public ReadOnlyLine Add(Line line) => throw null;
+        public ReadOnlyPoint Add(Point point) => throw null;
+        public ReadOnlyPolygon Add(Polygon polygon) => throw null;
+        public ReadOnlyText Add(Text text) => throw null;
+        public void Add(IEnumerable<Entity> entities) => throw null;
+        public void Add(Entity entity, IDictionary<string, IDictionary<string, object>> databaseAttributes) => throw null;
+        public void Delete(Entity entity) => throw null;
+        public void Delete(IEnumerable<Entity> entities) => throw null;
+        public void Dispose() => throw null;
+        public IDictionary<string, IDictionary<string, object>> GetDatabaseAttributes(Entity entity) => throw null;
+        public IEnumerator<Entity> GetEnumerator() => throw null;
+        IEnumerator IEnumerable.GetEnumerator() => throw null;
+        public string Wkt => throw null;
+        public IDictionary<string, int> DatabaseTables => throw null;
         public bool CanWrite {  get; }
         public bool CanRead {  get; }
-        public string Path { get; }
+        public string Path => throw null;
     }
 }
