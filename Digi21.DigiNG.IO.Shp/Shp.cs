6a4e4a2b8f920c05ddefdcaefac31c9ef572c6f5
José Ángel <joseangelmt@outlook.com>
2021-12-10 13:39:40 +0100

    IReadOnlyDrawingFile implements IWindow3D digi21/TareasDigi3D#432

--- Digi21.DigiNG.IO.Shp/Shp.cs
+++ Digi21.DigiNG.IO.Shp/Shp.cs
@@ -3,6 +3,7 @@ using Digi21.DigiNG.Entities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Digi21.Math;
 using Digi21.Tasks;
 
 namespace Digi21.DigiNG.IO.Shp
@@ -17,18 +18,18 @@ namespace Digi21.DigiNG.IO.Shp
         public Shp(string path, DigiTab.DigiTab digiTab) => throw null;
         public Shp(string path, CodePage codePage) => throw null;
         public Shp(string path, DelegateGetWkt GetWkt) => throw null;
-        public Shp(string path,  bool readOnly) => throw null;
+        public Shp(string path, bool readOnly) => throw null;
         public Shp(string path, DigiTab.DigiTab digiTab, CodePage codePage) => throw null;
         public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt) => throw null;
-        public Shp(string path, DigiTab.DigiTab digiTab,  bool readOnly) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, bool readOnly) => throw null;
         public Shp(string path, DelegateGetWkt GetWkt, CodePage codePage) => throw null;
-        public Shp(string path, DelegateGetWkt GetWkt,  bool readOnly) => throw null;
-        public Shp(string path,  bool readOnly, CodePage codePage) => throw null;
+        public Shp(string path, DelegateGetWkt GetWkt, bool readOnly) => throw null;
+        public Shp(string path, bool readOnly, CodePage codePage) => throw null;
         public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt, CodePage codePage) => throw null;
-        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt,  bool readOnly) => throw null;
-        public Shp(string path, DigiTab.DigiTab digiTab,  bool readOnly, CodePage codePage) => throw null;
-        public Shp(string path, DelegateGetWkt GetWkt,  bool readOnly, CodePage codePage) => throw null;
-        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt,  bool readOnly, CodePage codePage) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt, bool readOnly) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, bool readOnly, CodePage codePage) => throw null;
+        public Shp(string path, DelegateGetWkt GetWkt, bool readOnly, CodePage codePage) => throw null;
+        public Shp(string path, DigiTab.DigiTab digiTab, DelegateGetWkt GetWkt, bool readOnly, CodePage codePage) => throw null;
         public ReadOnlyComplex Add(Complex complex) => throw null;
         public void Add(Entity entity) => throw null;
         public ReadOnlyLine Add(Line line) => throw null;
@@ -43,9 +44,31 @@ namespace Digi21.DigiNG.IO.Shp
         IEnumerator IEnumerable.GetEnumerator() => throw null;
         public string Wkt => throw null;
         public IDictionary<string, int> DatabaseTables => throw null;
-        public bool CanWrite {  get; }
-        public bool CanRead {  get; }
+        public bool CanWrite { get; }
+        public bool CanRead { get; }
         public string Path => throw null;
         public string ConnectionString => throw null;
+        #region IWindow3D
+        public Point3D W => throw null;
+        public Point3D SW => throw null;
+        public Point3D S => throw null;
+        public Point3D SE => throw null;
+        public Point3D E => throw null;
+        public Point3D NE => throw null;
+        public Point3D N => throw null;
+        public Point3D NW => throw null;
+        public Point3D Center => throw null;
+        public double Depth => throw null;
+        public double Height => throw null;
+        public double Width => throw null;
+        public bool Valid => throw null;
+        public double Zmax => throw null;
+        public double Ymax => throw null;
+        public double Xmax => throw null;
+        public double Zmin => throw null;
+        public double Ymin => throw null;
+        public double? Xmin => throw null;
+        #endregion
     }
 }
+
