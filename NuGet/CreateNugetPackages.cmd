52b59aee225392304b1076be77a125a694a12702
José Ángel <joseangelmt@outlook.com>
2023-12-15 08:27:15 +0100

    Revert "Eliminada carpeta innecesaria"
    This reverts commit e29cd02c7415f7e1ce37942f890eb239a0566975.

---
+++ NuGet/CreateNugetPackages.cmd
@@ -0,0 +1,3 @@
+@echo off
+nuget.exe pack Digi21.DigiNG.IO.Shp.nuspec
+nuget.exe sign Digi21.DigiNG.IO.Shp.22.3.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS" -TimeStamper http://timestamp.comodoca.com?td=sha256.
